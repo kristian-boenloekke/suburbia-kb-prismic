3b5cda8c83a90676ab068e1bd9591bf1d24da975
Kristian Bønløkke <krbonlokke@gmail.com>
2025-02-17 20:56:43 +0100

    update Bounded.tsx

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,28 +1,56 @@
-import { CSSProperties, ElementType, ReactNode } from "react";
+// import { CSSProperties, ElementType, ReactNode } from "react";
+// import clsx from "clsx";
+
+// type BoundedProps = {
+//   as?: ElementType;
+//   className?: string;
+//   style?: CSSProperties;
+//   children: ReactNode;
+// };
+
+// export function Bounded({
+//   as: Comp = "section",
+//   className,
+//   children,
+//   ...restProps
+// }: BoundedProps) {
+  
+//   return (
+//     <Comp
+//       className={clsx(
+//         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
+//         className
+//       )}
+//       {...restProps}
+//     >
+//       <div className="mx-auto w-full max-w-6xl">{children}</div>
+//     </Comp>
+//   );
+// }
+
+import { CSSProperties, ReactNode } from "react";
 import clsx from "clsx";
 
 type BoundedProps = {
-  as?: ElementType;
   className?: string;
   style?: CSSProperties;
   children: ReactNode;
 };
 
 export function Bounded({
-  as: Comp = "section",
   className,
   children,
   ...restProps
 }: BoundedProps) {
   return (
-    <Comp
+    <section
       className={clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-        className,
+        className
       )}
       {...restProps}
     >
       <div className="mx-auto w-full max-w-6xl">{children}</div>
-    </Comp>
+    </section>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,18 +8,18 @@ import { FooterPhysics } from "./FooterPhysics"
 export async function Footer() {
   const client = createClient()
   const settings = await client.getSingle('settings')
-  const boardTextureURLs = settings.data.footer_skateboards.map((item) => asImageSrc(item.skateboard, {h: 600}))
-    .filter((url):url is string => Boolean(url))
+  const boardTextureURLs = settings.data.footer_skateboards.map((item) => asImageSrc(item.skateboard, { h: 600 }))
+    .filter((url): url is string => Boolean(url))
 
   return (
     <footer className="bg-texture bg-zinc-900 text-white overflow-hidden">
-      
+
       <div className="relative h-[75vh] ~p-10/16 md:aspect-auto">
         <PrismicNextImage
           field={settings.data.footer_image}
           alt=""
           fill
-          className="object-cover" 
+          className="object-cover"
           width={1200}
         />
 
@@ -27,15 +27,17 @@ export async function Footer() {
         {/* mix-blend-exclusion sætter kontrast opposite af background */}
         <FooterPhysics boardTextureURLs={boardTextureURLs} className="absolute inset-0 overflow-hidden" />
       </div>
-      <Bounded as="nav">
-        <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
-          {settings.data.navigation.map(item => (
-            <li key={item.link.text} className="hover:underline">
+      <Bounded>
+        <nav>
+          <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
+            {settings.data.navigation.map(item => (
+              <li key={item.link.text} className="hover:underline">
 
-              <PrismicNextLink field={item.link} />
-            </li>
-          ))}
-        </ul>
+                <PrismicNextLink field={item.link} />
+              </li>
+            ))}
+          </ul>
+        </nav>
       </Bounded>
     </footer>
   )
